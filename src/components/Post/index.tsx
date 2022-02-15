1e8913d7b1cf7c84b8b96631e30a84f47ce49fdf
devlulcas <lucasalvesrego.contato@gmail.com>
2022-02-14 22:25:25 -0300

    add: request to placeholder api

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function Post() {
+  return (
+    <article className="card">
+      <div>
+        <img src="https://via.placeholder.com/150/92c952" alt="" />
+      </div>
+      <h3>Título</h3>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </article>
+  );
+}
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,11 +1,31 @@
 import Title from "../../components/Title";
 import "./styles.css";
+import Post from "../../components/Post";
+import { useEffect, useState } from "react";
 
 export default function Blog() {
+  const [posts, setPosts] = useState([]);
+  const [seePost, setSeePost] = useState(false);
+
+  const url = "https://jsonplaceholder.typicode.com/posts/";
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => setPosts(json));
+  }, [seePost]);
+
   return (
     <>
       <section className="blog">
         <Title>BLOG</Title>
+        <button onClick={() => setSeePost(true)}>Ver postagens</button>
+
+        <section className="blog-container">
+          {posts.map((post) => {
+            return <Post />;
+          })}
+        </section>
       </section>
     </>
   );
