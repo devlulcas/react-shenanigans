83264e53f51b222db0065f78e7ba5c60e6a5c05f
devlulcas <lucasalvesrego.contato@gmail.com>
2022-02-21 22:12:00 -0300

    fix: pixelated graphics

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -2,8 +2,11 @@ import "./styles.css";
 import raccoon from "../../assets/raccoon.png";
 import { useRef } from "react";
 
+type Canvas = HTMLCanvasElement | null | undefined;
+type Context2d = CanvasRenderingContext2D | null | undefined;
+
 type drawProps = {
-  canvas: HTMLCanvasElement | null | undefined;
+  canvas: Canvas;
   image: any;
   event: any;
 };
@@ -18,21 +21,42 @@ function getMousePosition(canvas: any, event: any) {
   };
 }
 
+function resize(ctx: Context2d) {
+  ctx!.canvas.width = window.innerWidth;
+  ctx!.canvas.height = window.innerHeight;
+}
+
+function clearCanvas(canvas: Canvas, ctx: Context2d) {
+  // Tamanho do canvas para limpeza
+  const canvasWidth = canvas?.width || 0;
+  const canvasHeight = canvas?.height || 0;
+
+  ctx?.clearRect(0, 0, canvasWidth, canvasHeight);
+}
+
+function createText(canvas: Canvas, ctx: Context2d, text: string) {
+  ctx!.font = "80px Montserrat";
+  ctx!.fillStyle = "#ffecd6";
+  ctx!.textAlign = "center";
+  const canvasWidth = (canvas?.width || 0) / 2;
+  const canvasHeight = (canvas?.height || 0) / 2;
+  ctx!.fillText(text, canvasWidth, canvasHeight);
+}
+
 function drawRaccoon({ canvas, image, event }: drawProps) {
   const ctx = canvas?.getContext("2d");
 
   let { x, y } = getMousePosition(canvas, event);
 
   // Ajustar a imagem
-  x = x - image.width * 2;
-  y = y - image.height;
+  x = x - image.width * 20;
+  y = y - image.height * 20;
 
-  // Tamanho do canvas para limpeza
-  const canvasWidth = canvas?.width || 0;
-  const canvasHeight = canvas?.height || 0;
+  resize(ctx);
 
-  ctx?.clearRect(0, 0, canvasWidth, canvasHeight);
-  ctx?.drawImage(image, x, y, 40, 40);
+  clearCanvas(canvas, ctx);
+  createText(canvas, ctx, "404");
+  ctx?.drawImage(image, x, y, 404, 404);
 }
 
 export default function NotFound() {
@@ -41,8 +65,17 @@ export default function NotFound() {
 
   return (
     <>
-      <img ref={image} width={"11px"} src={raccoon} alt="guaxinim" />
+      <img
+        className="racoon"
+        ref={image}
+        width={"11px"}
+        src={raccoon}
+        alt="guaxinim"
+      />
+
       <canvas
+        width="404px"
+        height="404px"
         ref={canvas}
         className="canvas"
         onMouseMove={(event) => {
