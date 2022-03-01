41e3ad859f7531437c554f27e6829fb2cf19f11f
lucas rego <lucasalvesrego.contato@gmail.com>
2022-03-01 10:07:39 -0300

    change: removed  responsability

--- src/pages/AnimePhrases/index.tsx
+++ src/pages/AnimePhrases/index.tsx
@@ -1,25 +1,11 @@
-import { useEffect, useState } from "react";
 import AnimeQuote from "../../components/AnimeQuote";
-import Spinner from "../../components/Spinner";
-import { Anime } from "../../model/Anime";
 import "./styles.css";
 
 export default function AnimePhrase() {
-  const [anime, setAnime] = useState<Anime>();
-
-  async function getAnime() {
-    const anime = await Anime.getAnimeFromApi();
-    setAnime(anime);
-  }
-
-  useEffect(() => {
-    getAnime();
-  }, []);
-
   return (
     <>
       <section className="blog anime-page">
-        {anime ? <AnimeQuote anime={anime} /> : <Spinner />}
+        <AnimeQuote />
       </section>
     </>
   );
