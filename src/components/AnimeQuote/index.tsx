f8db2f6b6fe91537283cd38abc4d7e0ca5d52236
lucas rego <lucasalvesrego.contato@gmail.com>
2022-03-01 10:08:02 -0300

    change: trying to make it work

--- src/components/AnimeQuote/index.tsx
+++ src/components/AnimeQuote/index.tsx
@@ -1,40 +1,63 @@
+import { useEffect, useState } from "react";
 import { Anime } from "../../model/Anime";
+import { IAnimeProperties } from "../../model/interface/Anime";
+import Spinner from "../Spinner";
 import "./styles.css";
 
-type AnimeQuoteProps = {
-  anime: Anime;
+const defaultState: IAnimeProperties = {
+  englishTitle: "Code Geass: Lelouch of the Rebellion",
+  quote:
+    "War is merely one method of diplomacy. However, it is the least efficient one.",
+  character: "Bismarck Waldstein",
+  japoneseTitle: "コードギアス 反逆のルルーシュ",
+  startDate: "2006-10-06",
+  endDate: "2007-07-29",
+  posterImage: "https://media.kitsu.io/anime/poster_images/1415/large.jpg",
+  coverImage: "https://media.kitsu.io/anime/cover_images/1415/small.jpg",
 };
 
-function AnimeQuote({ anime }: AnimeQuoteProps) {
+function AnimeQuote() {
+  const [anime, setAnime] = useState<Anime | IAnimeProperties>();
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    Anime.getAnimeFromApi().then(anime => setAnime(anime || defaultState))
+    setLoading(false);
+  }, []);
+
+  if (loading) return <Spinner />;
+
   return (
     <article className="anime-article">
-      {console.log(anime.coverImage)}
-
-      <img className="cover" src={anime.coverImage} alt={anime.englishTitle} />
+      <img
+        className="cover"
+        src={anime?.coverImage}
+        alt={anime?.englishTitle}
+      />
       <img
         className="poster"
-        src={anime.posterImage}
-        alt={anime.englishTitle}
+        src={anime?.posterImage}
+        alt={anime?.englishTitle}
       />
 
       <div className="show">
         <h3 className="anime-title">
-          {anime.japoneseTitle}
-          <p>{anime.englishTitle}</p>
+          {anime?.japoneseTitle}
+          <p>{anime?.englishTitle}</p>
         </h3>
 
         <div className="show-lifetime">
-          <time dateTime={anime.startDate}>
-            <span>Start:</span> {anime.startDate}
+          <time dateTime={anime?.startDate}>
+            <span>Start:</span> {anime?.startDate}
           </time>
-          <time dateTime={anime.endDate}>
-            <span>End:</span> {anime.endDate}
+          <time dateTime={anime?.endDate}>
+            <span>End:</span> {anime?.endDate}
           </time>
         </div>
 
         <blockquote>
-          {anime.quote}
-          <p>~ {anime.character}</p>
+          {anime?.quote}
+          <p>~ {anime?.character}</p>
         </blockquote>
       </div>
     </article>
