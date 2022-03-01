450a848eed744227ecbe449cd1247be0d62f3f35
lucas rego <lucasalvesrego.contato@gmail.com>
2022-03-01 10:06:01 -0300

    add: interface for examples

---
+++ src/model/interface/Anime.ts
@@ -0,0 +1,10 @@
+export interface IAnimeProperties {
+  englishTitle: string;
+  quote: string;
+  character: string;
+  japoneseTitle: string;
+  startDate: string;
+  endDate: string;
+  posterImage: string;
+  coverImage: string;
+}
